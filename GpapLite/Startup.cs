c7b7177c0a54b539dcc829855d7c8bcbb781c746
Brian Okanga <briannokanga@gmail.com>
2020-10-07 20:25:49 +0300

    Moved Data DbContext for User identity and added migrations

--- GpapLite/Startup.cs
+++ GpapLite/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using GpapLite.User.Data;
+using GpapLite.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,9 +26,9 @@ namespace GpapLite
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDbContext<GpapDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("GpapConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("GpapLiteConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
