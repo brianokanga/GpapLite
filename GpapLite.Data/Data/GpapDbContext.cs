408a367a21ceafe293801db5823a09259301a191
Brian Okanga <briannokanga@gmail.com>
2020-10-07 18:55:24 +0300

    Add project files.

---
+++ GpapLite.Data/Data/GpapDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace GpapLite.Data.Data
+{
+    public class GpapDbContext : DbContext
+    {
+        public GpapDbContext(DbContextOptions<GpapDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
